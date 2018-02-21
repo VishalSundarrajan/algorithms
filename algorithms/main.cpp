7a0790c495b44b3b2879446204d8e0824bc0fb33
Vishal <VishalSundarrajan93@gmail.com>
2018-02-21 17:42:19 -0500

    Initial Commit

---
+++ algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by Vishal on 2/21/18.
+//  Copyright © 2018 Vishal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
